35bcf193e46c127956b7eb440ac254d2f5c4f75f
Strashman <strashman@gmail.com>
2024-10-09 23:37:39 +0300

    Add files via upload Добавлены персонажи Кабан Пятак и Колибри Флит.

---
+++ src/Colibri.java
@@ -0,0 +1,17 @@
+public class Colibri extends Bird implements IFlyer{
+    public Colibri(String name, int age, double wingsSize) {
+    super(name, age, wingsSize);
+    }
+
+
+    @Override
+    public void move() {}
+
+
+
+        public void fly(){
+
+        System.out.println(this + " очень быстро летает с ветки на ветку ");
+        }
+    }
+
