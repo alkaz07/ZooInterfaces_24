8eb80c54c3ecc2eb9664a6df64da468e213f3616
Sergei-Lebedev1 <pooshok420@gmail.com>
2024-10-10 19:47:16 +0300

    исправил Фламинго

--- src/zoo/birds/Flamingo.java
+++ src/zoo/birds/Flamingo.java
@@ -16,13 +16,22 @@ public class Flamingo extends Bird implements IFlyer, IWalker {
     public void move() {
 
         walk();
-
-
     }
 
     @Override
     public void fly() {
         System.out.println("Фламинго летит далеко");
+    }
+
+    @Override
+    public void takeoff(String from) {
+        System.out.println(color + " " +name+ " Взлетает с поверхности мелководного водоема");
+    }
+
+    @Override
+    public void land(String to) {
+        System.out.println(color + " " +name+ " Приземлился и чистит перышки");
+
     }
 
     @Override
