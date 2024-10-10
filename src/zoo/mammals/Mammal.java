55f6759cee1121ec0ced537db751fd499811ed29
Strashman <>
2024-10-10 19:11:17 +0300

    update 1010

--- src/zoo/mammals/Mammal.java
+++ src/zoo/mammals/Mammal.java
@@ -3,7 +3,22 @@ package zoo.mammals;
 import zoo.Animal;
 
 public abstract class Mammal extends Animal {
+    protected String name;
+    int age;
+
     public Mammal(String name, int age) {
         super(name, age);
+
     }
+    @Override
+    public String toString() {
+        return "zoo.Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+    public abstract void move();
+
+
 }
+
