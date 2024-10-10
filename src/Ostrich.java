3f3bcc868d2d3cc2ee716101428fcbb253f26540
acerkuv <acerkuv@gmail.com>
2024-10-10 08:33:58 +0300

    Добавил класс Ostrich ("Страус"), наследует от Birds, имплеметирует IWalker. Добавлно новое поле класса Height

---
+++ src/Ostrich.java
@@ -0,0 +1,18 @@
+public class Ostrich extends Bird implements IWalker{
+    public double height;
+
+    public Ostrich(String name, int age, double wingsSize, double height) {
+        super(name, age, wingsSize);
+        this.height = height;
+    }
+
+    @Override
+    public void move() {
+        walk();
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Я страус и только хожу");
+    }
+}
