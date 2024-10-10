d27bc3cf59b840238cc267bce6f3a687928eda26
Ruslan7173 <lifecol57@gmail.com>
2024-10-10 19:44:19 +0300

    Изменил Seagull добавлен взлет и посадка

--- src/zoo/birds/Seagull.java
+++ src/zoo/birds/Seagull.java
@@ -15,4 +15,16 @@ public class Seagull extends Bird implements IFlyer {
     public void fly(){
         System.out.println(this + "чайка летит над морем");
     }
+
+    @Override
+    public void takeoff(String from) {
+        System.out.println("чайка взлетает с поверхности моря "+from);
+
+    }
+
+    @Override
+    public void land(String to) {
+        System.out.println("чайка садится на скалы "+to);
+
+    }
 }
