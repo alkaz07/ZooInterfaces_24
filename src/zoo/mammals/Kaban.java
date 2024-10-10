001f4ddb5b0c7d994f56348c3dd71a0f92b49e0e
usdim <csnlabs@hotmail.com>
2024-10-10 19:10:04 +0300

    Использовали интерфеейс Irunner в кабане

--- src/zoo/mammals/Kaban.java
+++ src/zoo/mammals/Kaban.java
@@ -1,21 +1,30 @@
 package zoo.mammals;
 
 import zoo.Animal;
+import zoo.IRunner;
 import zoo.IWalker;
 
 
-public class Kaban extends Mammal implements IWalker {
+public class Kaban extends Mammal implements IWalker, IRunner {
     public Kaban(String name, int age) {
         super(name, age);
     }
 
     @Override
-    public void move() {walk();
+    public void move() {
+        walk();
 
     }
 
     @Override
     public void walk() {
-        System.out.println(this+"Кабан роется в желудях и похрюкивает");
+        System.out.println(this + "Кабан роется в желудях и похрюкивает");
+    }
+
+    @Override
+    public void run() {
+        {
+            System.out.println(this + "Кабан Василий несется по лесу с радостным хрюканьем ");
+        }
     }
 }
